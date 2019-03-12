750f6db5a4f6afc9e55224da0c5d69cab6c22203
AbekNarynov <vhkeba@gmail.com>
2019-03-13 00:12:08 +0300

    feat(Models): Created new models and migrations: AboutPage, Portfolio, PricePage

--- database/migrations/2019_03_11_194809_create_portfolios_table.php
+++ database/migrations/2019_03_11_194809_create_portfolios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePortfoliosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('portfolios', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->text('image');
+            $table->string('language')->index();
+            $table->unique('language');
+            $table->integer('likes');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('portfolios');
     }
 }
